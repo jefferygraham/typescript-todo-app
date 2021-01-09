ad3685500408f3b748b214fd8014cfc6ba58d40b
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-09 04:03:20 -0500

    add bodyParser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import todoRoutes from './routes';
@@ -8,6 +9,7 @@ const app: Express = express();
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(bodyParser());
 app.use(todoRoutes);
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.peqjw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
